9bbb369b7f7166c5160ebcb8a4f27261418d1fe1
sbogacz <steven.bogacz@gmail.com>
2018-08-24 20:38:56 -0600

    add sample usage to outputs
    add sample usage to outputs
    
    add sample usage to outputs
    
    cleanup formatting, round 1
    
    cleanup formatting, round 2
    
    cleanup formatting, round 3
    
    cleanup formatting, round 4
    
    cleanup formatting, round 5
    
    cleanup formatting, round 6
    
    cleanup formatting, round 7
    
    cleanup formatting, round 8

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,11 @@
 data "aws_region" "current" {}
 
 locals {
-  values = {
-    bucket         = "${aws_s3_bucket.terraform_state.arn}"
-    region         = "${data.aws_region.current.name}"
-    dynamodb_table = "${aws_dynamodb_table.dynamodb_terraform_state_lock.id}"
-    key            = "example/path/to/state"
-  }
-
-  #tf_backend_sample = "${format("terraform {\n\t backend \\\"s3\\\" {\n %s}\n}\n", jsonencode(values))}"
-  tf_backend_sample = "${format("terraform {\n  backend \"s3\" {\n %s}\n}\n", jsonencode(values))}"
+  bucket_val         = "${format("bucket = \"%s\"", aws_s3_bucket.terraform_state.id)}"
+  region_val         = "${format("region = \"%s\"", data.aws_region.current.name)}"
+  dynamodb_table_val = "${format("dynamodb_table = \"%s\"", aws_dynamodb_table.dynamodb_terraform_state_lock.id)}"
+  key_val            = "key = \"example/path/to/state\""
+  values             = "${format("      %s\n      %s\n      %s\n      %s\n", local.bucket_val, local.region_val, local.dynamodb_table_val, local.key_val)}"
 }
 
 output "state_s3_bucket_name" {
@@ -18,4 +14,9 @@ output "state_s3_bucket_name" {
 
 output "state_s3_bucket_arn" {
   value = "${aws_s3_bucket.terraform_state.arn}"
+}
+
+output "sample_usage" {
+  description = "this shows an example of how to use the created resources for a terraform backend"
+  value       = "${format("\n\n\nterraform {\n  backend \"s3\" {\n%s  }\n}", local.values)}"
 }
