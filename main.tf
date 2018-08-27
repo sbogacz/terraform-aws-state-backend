971c472943ccab918d6eb55a8b5f26a314a8650e
sbogacz <steven.bogacz@gmail.com>
2018-08-27 11:00:34 -0600

    support configurable DynamoDB table capacity, ignore .terraform

--- main.tf
+++ main.tf
@@ -44,8 +44,8 @@ resource "aws_dynamodb_table" "dynamodb_terraform_state_lock" {
   # https://www.terraform.io/docs/backends/types/s3.html#dynamodb_table
   hash_key = "LockID"
 
-  read_capacity  = 20
-  write_capacity = 20
+  read_capacity  = "${var.lock_table_read_capacity}"
+  write_capacity = "${var.lock_table_write_capacity}"
 
   attribute {
     name = "LockID"
